7c1cf767f22615e702dd74781603f54a3b1ec2e7
Hugo Moraes Bonatto <hugomoraes_@hotmail.com>
2020-08-26 22:10:32 -0300

    StatusBar

--- android/src/index.js
+++ android/src/index.js
@@ -1,6 +1,8 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+
 import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler';
-import React from 'react';
 
 import './config/ReactotronConfig';
 import Routes from './routes';
@@ -8,6 +10,7 @@ import Routes from './routes';
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <Routes />
     </NavigationContainer>
   );
